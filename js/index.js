499e1b234c47b2b1a61d1bf099a2767a30487740
Soham manjrekar <mrsohammanjrekar@gmail.com>
2023-04-08 10:36:02 +0530

    update

--- js/index.js
+++ js/index.js
@@ -1,84 +1,40 @@
-const resultElement = document.getElementById("result");
-const lengthElement = document.getElementById("length");
-const uppercaseElement = document.getElementById("uppercase");
-const lowercaseElement = document.getElementById("lowercase");
-const numbersElement = document.getElementById("numbers");
-const symbolsElement = document.getElementById("symbols");
-const generateElement = document.getElementById("generate");
-const clipboardElement = document.getElementById("clipboard");
+const inputEl = document.getElementById('input')
+const infoTextEl = document.getElementById('info-text')
+const meaningContainerEl = document.getElementById('meaning-container')
+const titleEl = document.getElementById('title')
+const meaningEl = document.getElementById('meaning')
+const audioEl = document.getElementById('audio')
 
-// Random functions
-// fromCharCode: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
-// ASCII codes: https://www.w3schools.com/charsets/ref_html_ascii.asp
-const getRandomLower = () =>
-  String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+async function fetchAPI(word) {
+  try {
+    infoTextEl.style.display = 'block'
+    meaningContainerEl.style.display = 'none'
+    infoTextEl.innerText = `Searching the meaning of "${word}"`
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    const result = await fetch(url).then((res) => res.json())
 
-const getRandomUpper = () =>
-  String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-
-const getRandomNumber = () =>
-  String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-
-const getRandomSymbol = () => {
-  const symbols = "!@#$%^&*(){}[]=<>/,.";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-};
-
-const randomFunctions = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomSymbol,
-  symbol: getRandomSymbol,
-};
-
-const createNotification = (message) => {
-  const notif = document.createElement("div");
-  notif.classList.add("toast");
-  notif.innerText = message;
-  document.body.appendChild(notif);
-  setTimeout(() => notif.remove(), 3000);
-};
-
-clipboardElement.addEventListener("click", () => {
-  const password = resultElement.innerText;
-  if (!password) return;
-  const textarea = document.createElement("textarea");
-  textarea.value = password;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand("copy");
-  textarea.remove();
-  createNotification("Password copied to clipboard!");
-});
-
-generateElement.addEventListener("click", () => {
-  const length = +lengthElement.value;
-  const hasLower = lowercaseElement.checked;
-  const hasUpper = uppercaseElement.checked;
-  const hasNumber = numbersElement.checked;
-  const hasSymbol = symbolsElement.checked;
-  resultElement.innerText = generatePassword(
-    hasLower,
-    hasUpper,
-    hasNumber,
-    hasSymbol,
-    length
-  );
-});
-
-const generatePassword = (lower, upper, number, symbol, length) => {
-  let generatedPassword = "";
-  const typesCount = lower + upper + number + symbol;
-  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
-    (item) => Object.values(item)[0]
-  );
-  if (typesCount === 0) return "";
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach((type) => {
-      const funcName = Object.keys(type)[0];
-      generatedPassword += randomFunctions[funcName]();
-    });
+    if (result.title) {
+      meaningContainerEl.style.display = 'block'
+      infoTextEl.style.display = 'none'
+      titleEl.innerText = word
+      meaningEl.innerText = 'N/A'
+      audioEl.style.display = 'none'
+    } else {
+      infoTextEl.style.display = 'none'
+      meaningContainerEl.style.display = 'block'
+      audioEl.style.display = 'inline-flex'
+      titleEl.innerText = result[0].word
+      meaningEl.innerText = result[0].meanings[0].definitions[0].definition
+      audioEl.src = result[0].phonetics[0].audio
+    }
+  } catch (error) {
+    console.log(error)
+    infoTextEl.innerText = `an error happened, try again later`
   }
-  const finalPassword = generatedPassword.slice(0, length);
-  return finalPassword;
-};
+}
+
+inputEl.addEventListener('keyup', (e) => {
+  if (e.target.value && e.key === 'Enter') {
+    fetchAPI(e.target.value)
+  }
+})
